8dec359b0906c6b0bdffb7dabed3b17dfb649f50
Zamir1907 <87573585+Zamir1907@users.noreply.github.com>
2026-08-16 09:36:49 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* ==================== CONFIG ==================== */
 const QRIS_IMAGE_URL = "https://cdn.aceimg.com/ZhkhxG9a8.jpg";
 const MERCHANT_NAME = "NAIKIN SOSMED";
-const LOGIN_BONUS = 9800;
+const LOGIN_BONUS = 13800;
 const MIN_DEPOSIT = 10000;
 
 const STORAGE_KEYS = {
