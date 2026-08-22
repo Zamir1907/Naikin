981f29a258f67b5dbb7b978a73b6249a6c8aba8d
Zamir1907 <87573585+Zamir1907@users.noreply.github.com>
2026-08-22 23:29:44 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* ==================== CONFIG ==================== */
 const QRIS_IMAGE_URL = "https://cdn.aceimg.com/ZhkhxG9a8.jpg";
 const MERCHANT_NAME = "NAIKIN SOSMED";
-const LOGIN_BONUS = 13800;
+const LOGIN_BONUS = 5500;
 const MIN_DEPOSIT = 10000;
 const QRIS_MIN_WAIT_MS = 15000; // min tunggu 15 detik sebelum konfirmasi QRIS
 
